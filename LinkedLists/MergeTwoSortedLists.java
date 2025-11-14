b5f160943654299b22169e6cc757a65b29f9e462
sreejap <sreeja605@gmail.com>
2025-11-14 12:19:22 -0500

    Add mergeTwoLists method for sorted list merging
    Implement mergeTwoLists method to merge two sorted linked lists.

---
+++ LinkedLists/MergeTwoSortedLists.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ // T(c) - o(n+m), S(C) - o(1)
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode() ;
+        ListNode resHead = res;
+        while (list1!= null && list2!=null){
+            if (list1.val <= list2.val) {
+                res.next = list1;                
+                list1 = list1.next;
+            } else {
+                res.next = list2;
+                list2 = list2.next;
+            }
+            res = res.next;
+        }
+        
+        if (list1!=null) { // append the remaining list to the res
+            res.next = list1;            
+        }
+
+        if (list2!=null){
+            res.next = list2;
+            
+        }
+        return resHead.next;
+    }
+}
