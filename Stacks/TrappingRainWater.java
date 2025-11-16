d48b6a692fb2aa6de512684c8c2e8be49d41287d
sreejap <sreeja605@gmail.com>
2025-11-16 12:45:36 -0500

    Add TrappingRainWater solution in Java
    Implement solution for trapping rainwater problem using a stack.

---
+++ Stacks/TrappingRainWater.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/trapping-rain-water/
+// T(C) - O(n), S(C) - O(n)
+class Solution {
+    public int trap(int[] height) {
+
+        int current =0;
+        int ans =0;
+        Deque <Integer> st = new ArrayDeque <>(); // simple que 
+        int len = height.length;
+        while (current < len){
+            while (!st.isEmpty() && height[current] > height[st.peek()]) {
+                // we need valley and we need left boundary 
+                // if height[current] > top of the monotic dec stack, we have a valley
+                int top = st.peek();
+                st.pop(); 
+                if (st.isEmpty()){
+                    break;
+                }
+
+                int leftBoundary = st.peek();
+                int distance = current - leftBoundary -1; 
+                int h = Math.min (height[leftBoundary], height[current]) - height [top];
+
+                ans += distance*h;
+            }
+            st.push(current++);
+        }
+        return ans;        
+    }
+}
